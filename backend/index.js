d16fd342eb5193b9073c6fa3ab5258c892047d85
gambitmis <gambitmis@gmail.com>
2021-07-28 15:56:29 +0000

    complete lesson

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const ifNotLoggedIn = (req, res, next) => {
   next();
 }
 
-const ifLoggedIn = (req,res,next) => {
+const ifLoggedIn = (req, res, next) => {
   if(req.session.isLoggedIn) {
     return res.redirect('/home');
   }
@@ -35,12 +35,13 @@ const ifLoggedIn = (req,res,next) => {
 }
 
 app.get('/', ifNotLoggedIn,(req,res,next) => {
+  console.log('get');
   dbConnection.execute("SELECT name FROM users WHERE id = ?", [req.session.userID])
-  .then(([row]) => {
-    res.render('home', {
-      name: row[0].name
+  .then(([rows]) => {
+      res.render('home', {
+        name: rows[0].name
     })
-  })
+  });
 })
 
 // Register Page
@@ -65,7 +66,7 @@ app.post('/reg', ifLoggedIn, [
       bcrypt.hash(user_pass,12).then((hash_pass) => {
         dbConnection.execute("INSERT INTO users (name, email, password) VALUES(?,?,?)",[user_name,user_email,hash_pass])
         .then(result => {
-          res.send(`Your Account has been Create Successfully <a herf="/">Login</a>`);
+          res.send(`Your Account has been Create Successfully <a href="/logout" class="btn btn-primary">login</a>`);
         }).catch(err => {
           if (err) throw err;
         })
@@ -83,6 +84,54 @@ app.post('/reg', ifLoggedIn, [
     }
   })
 
+app.post('/', ifLoggedIn, [
+    body('user_email').custom((value) => {
+        return dbConnection.execute("SELECT email FROM users WHERE email = ?", [value])
+        .then(([rows]) => {
+          if (rows.length == 1) {
+            console.log("found")
+            return true;
+          }
+          return Promise.reject('Invalid Email Address')
+        });
+    }),
+    body('user_pass','Password is empty').trim().not().isEmpty(),
+], (req,res) => {
+  const validation_result = validationResult(req);
+  const { user_pass, user_email } = req.body;
+  if (validation_result.isEmpty()){
+    dbConnection.execute("SELECT * FROM users WHERE email = ?", [user_email])
+    .then(([rows]) => {
+      bcrypt.compare(user_pass, rows[0].password).then(compare_result => {
+        if (compare_result === true){
+          req.session.isLoggedIn = true;
+          req.session.userID = rows[0].id;
+          res.redirect('/');
+        }else{
+          res.render('login-register', {
+            login_errors: ['Invalid Password']
+          })
+        }
+      }).catch(err => {
+        if (err) throw err;
+      })
+    }).catch(err => {
+      if (err) throw err;
+    })
+  } else {
+    let allErrors = validation_result.errors.map((error) => {
+      return error.msg;
+    })
+    res.render('login-register',{
+      login_errors: allErrors
+    })
+  }
+}) 
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  res.redirect('/');
+})
 app.listen(port, () => {
   console.log(`listen on port ${port}`)
 })
