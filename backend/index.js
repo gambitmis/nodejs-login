f71c982710b3d6372101d306ad58da60b413909d
gambitmis <gambitmis@gmail.com>
2021-07-28 11:12:36 +0000

    register ok

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const dbConnection = require('./database');
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, Result } = require('express-validator');
 
 const app = express()
 const port = 3000
@@ -27,6 +27,13 @@ const ifNotLoggedIn = (req, res, next) => {
   next();
 }
 
+const ifLoggedIn = (req,res,next) => {
+  if(req.session.isLoggedIn) {
+    return res.redirect('/home');
+  }
+  next();
+}
+
 app.get('/', ifNotLoggedIn,(req,res,next) => {
   dbConnection.execute("SELECT name FROM users WHERE id = ?", [req.session.userID])
   .then(([row]) => {
@@ -35,6 +42,46 @@ app.get('/', ifNotLoggedIn,(req,res,next) => {
     })
   })
 })
+
+// Register Page
+app.post('/reg', ifLoggedIn, [
+  body('user_email', 'Invalid Email Address!').isEmail().custom((value) => {
+    return dbConnection.execute('SELECT email FROM users WHERE email = ?',[value])
+    .then(([rows]) => {
+      if (rows.length > 0){ // Query found exiting Email
+        return Promise.reject('This email already used');
+      }
+      return true;
+    })
+  }),
+  body('user_name','Username is empty!').trim().not().isEmpty(),
+  body('user_pass','The password must be of minimun length 6 character').trim().isLength({ min:6 })
+], // end of post data validation
+  (req,res,next) => {
+    const validation_result = validationResult(req);
+    const { user_name, user_pass, user_email } = req.body;
+
+    if (validation_result.isEmpty()){
+      bcrypt.hash(user_pass,12).then((hash_pass) => {
+        dbConnection.execute("INSERT INTO users (name, email, password) VALUES(?,?,?)",[user_name,user_email,hash_pass])
+        .then(result => {
+          res.send(`Your Account has been Create Successfully <a herf="/">Login</a>`);
+        }).catch(err => {
+          if (err) throw err;
+        })
+      }).catch(err => {
+        if (err) throw err;
+      })
+    } else {
+      let allErrors = validation_result.errors.map((error) => {
+        return error.msg;
+      })
+      res.render('login-register', {
+        register_error: allErrors,
+        old_data: req.body
+      })
+    }
+  })
 
 app.listen(port, () => {
   console.log(`listen on port ${port}`)
